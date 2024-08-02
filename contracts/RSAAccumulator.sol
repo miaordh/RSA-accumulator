c2968bce168ac7ce986f759f4dd069975acce404
miaordh <miaordh@foxmail.com>
2024-08-02 10:59:15 +0800

    Update RSAAccumulator.sol
    Move equal() from BytesLib to this file for compilation simplicity

--- contracts/RSAAccumulator.sol
+++ contracts/RSAAccumulator.sol
@@ -1,17 +1,61 @@
 pragma solidity ^0.5.0;
 
-import "./BytesLib.sol";
 
 contract RSAAccumulator {
 
-    using BytesLib for bytes;
 
     bytes acc_post;
     bytes modulus;
 
+
     constructor (bytes memory _modulus, bytes memory _acc_post) public {
-        acc_post = _acc_post;
         modulus = _modulus;
+        acc_post = _acc_post;
+    }
+
+    event Result(bool success);
+
+    function equal(bytes memory _preBytes, bytes memory _postBytes) internal pure returns (bool) {
+        bool success = true;
+
+        assembly {
+            let length := mload(_preBytes)
+
+        // if lengths don't match the arrays are not equal
+            switch eq(length, mload(_postBytes))
+            case 1 {
+            // cb is a circuit breaker in the for loop since there's
+            //  no said feature for inline assembly loops
+            // cb = 1 - don't breaker
+            // cb = 0 - break
+                let cb := 1
+
+                let mc := add(_preBytes, 0x20)
+                let end := add(mc, length)
+
+                for {
+                    let cc := add(_postBytes, 0x20)
+                // the next line is the loop condition:
+                // while(uint(mc < end) + cb == 2)
+                } eq(add(lt(mc, end), cb), 2) {
+                    mc := add(mc, 0x20)
+                    cc := add(cc, 0x20)
+                } {
+                // if any of these checks fails then arrays are not equal
+                    if iszero(eq(mload(mc), mload(cc))) {
+                    // unsuccess:
+                        success := 0
+                        cb := 0
+                    }
+                }
+            }
+            default {
+            // unsuccess:
+                success := 0
+            }
+        }
+
+        return success;
     }
 
     function verify(bytes memory base, bytes32 e) public returns (bool) {
@@ -53,7 +97,7 @@ contract RSAAccumulator {
             mstore(0x40, add(p, add(0x20, base_length)))
         // o := p
         }
-
-        return p.equal(acc_post);
+        emit Result(equal(p, acc_post));
+        return equal(p, acc_post);
     }
 }
