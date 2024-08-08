26050c0e2905da80d409647d568edaef94238b65
miaordh <miaordh@foxmail.com>
2024-08-08 09:29:49 +0800

    Update implementation.py

--- implementation.py
+++ implementation.py
@@ -1,18 +1,20 @@
 import main
-import hashlib
-import random
 import sys
 import time
 import merkletools
-from helpfunctions import hash_to_prime, write_list_to_file, extract_merkle_proof, create_random_list, hash_integers, to_padded_num_str, make_even_size
+from helpfunctions import hash_to_prime, write_list_to_file, write_two_d_array_to_file, extract_merkle_proof, create_random_list, hash_integers, to_padded_num_str, make_even_size
+from dotenv import dotenv_values
 
-RANDOM_LIST_SIZE = 1000
-SINGLE_PROVE_INDEX = 8 # Don't forget to change it in interact.js as well
-BATCH_PROVE_SIZE = 900
+
+config = dotenv_values(".env")
+RANDOM_LIST_SIZE = int(config["RANDOM_LIST_SIZE"])
+RANDOM_LIST_MAXIMUM_LENGTH = int(config["RANDOM_LIST_MAXIMUM_LENGTH"])
+SINGLE_PROVE_INDEX = int(config["SINGLE_PROVE_INDEX"])
+BATCH_PROVE_SIZE = int(config["BATCH_PROVE_SIZE"])
 sys.set_int_max_str_digits(1000000)
 
 # Create random list
-random_list = create_random_list(RANDOM_LIST_SIZE)
+random_list = create_random_list(RANDOM_LIST_SIZE, RANDOM_LIST_MAXIMUM_LENGTH)
 random_list_hashed = hash_integers(random_list) # Merkle tree needs hashed elements
 # Convert the random list into hexadecimal numbers
 random_list_hex = [] 
@@ -112,9 +114,18 @@ print("Now check a group of elements with indices [0, {}]\n".format(BATCH_PROVE_
 startProve = time.time()
 A_batch_proof = main.batch_prove_membership(A0, S, batch_checked, n)
 stopProve = time.time()
+
+with open("./textlist/A_batch_proof_hex.txt", 'w') as A_batch_proof_hex_file:
+    A_batch_proof_hex_file.write(to_padded_num_str(A_batch_proof, 384))
+
 batch_nonce = []
 for key in batch_checked:
     batch_nonce.append(S[key])
+
+batch_elements_hashed_to_prime = main.calculate_primes_product(batch_checked, batch_nonce)
+with open("./textlist/batch_elements_hashed_to_prime.txt", 'w') as batch_elements_hashed_to_prime_file:
+    batch_elements_hashed_to_prime_file.write(to_padded_num_str(batch_elements_hashed_to_prime, 32))
+
 
 startVerify = time.time()
 batch_membership = main.batch_verify_membership(A, batch_checked, batch_nonce, A_batch_proof, n)
@@ -136,11 +147,17 @@ print("Batch check. Using Python dictionary key search.\nMember? {}.\nChecking t
 membership = True
 prove_time = 0.0
 verify_time = 0.0
+batch_mt_proof_hashes = []
+batch_mt_proof_sides = []
 for i in range(BATCH_PROVE_SIZE):
     tik = time.time()
     mt_proof = mt.get_proof(i)
     tok = time.time()
     prove_time += tok - tik
+    proof_side, proof_hash = extract_merkle_proof(mt_proof)
+    batch_mt_proof_hashes.append(proof_hash)
+    batch_mt_proof_sides.append(proof_side)
+
     checked_leaf = mt.get_leaf(i)
     tik = time.time()
     mt_is_valid = mt.validate_proof(mt_proof, checked_leaf, mt_root)
@@ -150,9 +167,7 @@ for i in range(BATCH_PROVE_SIZE):
         membership = False
         break
 
+write_two_d_array_to_file(batch_mt_proof_hashes, "./textlist/batch_proof_hashes.txt")
+write_two_d_array_to_file(batch_mt_proof_sides, "./textlist/batch_proof_sides.txt")
+
 print("Batch check. Using Merkle tree validation.\nMember? {}.\nGenerating proof takes {} seconds ({} seconds per checked element).\nVerifying takes {} seconds ({} seconds per checked element).\n".format(membership, prove_time, prove_time / BATCH_PROVE_SIZE, verify_time, verify_time / BATCH_PROVE_SIZE))
-
-
-
-
-
